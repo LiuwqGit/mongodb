e5495af77b95f3f8456228297c1eaa6183ddb7f4
liuwq_yonyou <772863869@qq.com>
2016-04-11 16:34:40 +0800

    modified Query

--- MongoDriver/Version141/Program.cs
+++ MongoDriver/Version141/Program.cs
@@ -15,36 +15,40 @@ namespace Version141
         private const string strconn = "mongodb://127.0.0.1:27017";
         //数据库名称
         private const string dbName = "cnblogs";
+        //集合名称
+        private const string dbCollection = "Users";
 
         static void Main(string[] args)
         {
-            Insert();
+            //Insert();
             //Update();
             //Delete();
+            Query();
+            Console.WriteLine("Query");
         }
         /// <summary>
         /// 初始化数据库文档
         /// </summary>
         /// <returns></returns>
-        private static MongoDatabase Init()
+        private static MongoCollection Init()
         {
             //创建数据库链接
             MongoServer server = new MongoClient(strconn).GetServer();
             //获得数据库cnblogs
             MongoDatabase db = server.GetDatabase(dbName);
-            return db;
+            MongoCollection col = db.GetCollection(dbCollection);
+            return col;
         }
         private static void Insert()
         {
-            MongoDatabase db = Init();
+            MongoCollection col = Init();
 
             for (int i = 0; i < 10; i++)
             {
                 Users users = new Users();
                 users.Name = "liusc" + i.ToString();
                 users.Sex = "man";
-                //获得Users集合，如果数据库中没有，系统会自动新建一个
-                MongoCollection col = db.GetCollection("Users");
+                //获得Users集合，如果数据库中没有，系统会自动新建一个 
                 //执行插入操作
                 col.Insert<Users>(users);
             }
@@ -52,9 +56,7 @@ namespace Version141
 
         private static void Update()
         {
-            MongoDatabase db = Init();
-            //获取Users集合
-            MongoCollection col = db.GetCollection("Users");
+            MongoCollection col = Init();
             //定义获取"Name"值为"liusc"的查询条件
             var query = new QueryDocument { { "Name", "liusc" } };
             //定义更新文档
@@ -65,13 +67,26 @@ namespace Version141
 
         private static void Delete()
         {
-            MongoDatabase db = Init();
-            //获取Users集合
-            MongoCollection col = db.GetCollection("Users");
+            MongoCollection col = Init();
             //定义获取"Name"值为"liusc"的查询条件
             var query = new QueryDocument { { "Name", "liusc" } };
-            //col.Remove(query);
-            col.RemoveAll();
+            col.Remove(query);//移除集合中满足条件query的数据
+            //col.RemoveAll();//移除集合全部数据
+        }
+
+        private static void Query()
+        {
+            MongoCollection col = Init();
+
+            //定义获取"Name"值为"liusc"的查询条件
+            var query = new QueryDocument { { "Name", "liusc0" } };
+            //查询全部集合里的数据
+            var result1 = col.FindAllAs<Users>();
+            //查询指定查询条件的第一条数据，查询条件可缺省。
+            var result2 = col.FindOneAs<Users>();
+            //查询指定查询条件的全部数据
+            var result3 = col.FindAs<Users>(query);
+
         }
 
         public class Users
